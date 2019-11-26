29070f12705b1f4da1ddb73ac5ae5c29e7497afb
Grashaar <grashaar@outlook.com>
2019-11-26 17:38:57 +0700

    Update example to use Addressables to load assets

--- Examples/Scripts/StartGame.cs
+++ Examples/Scripts/StartGame.cs
@@ -1,16 +1,45 @@
-﻿using UnityEngine;
+﻿// Include UnityCore.Addressables assembly
+
+#if USE_ADDRESSABLES
+using System;
+using System.Collections;
+using UnityEngine.AddressableAssets;
+#endif
+
+using UnityEngine;
 using UnuGames;
+
+#if USE_ADDRESSABLES
+using UnityObject = UnityEngine.Object;
+#endif
 
 public class StartGame : MonoBehaviour
 {
-    // Use this for initialization
     private void Start()
     {
+#if USE_ADDRESSABLES
+        UIManAssetLoader.RegisterLoadMethodGameObject(LoadGameObject);
+        UIManAssetLoader.RegisterLoadMethodSprite(LoadSprite);
+        UIManAssetLoader.RegisterLoadMethodTexture2D(LoadTexture2D);
+#endif
+
         UIMan.Instance.ShowScreen<UIMainMenu>();
     }
 
-    // Update is called once per frame
-    private void Update()
+#if USE_ADDRESSABLES
+    private static IEnumerator LoadGameObject(string key, Action<string, UnityObject> callback)
     {
+        yield return AddressableManager.Load<GameObject>(key, callback);
     }
+
+    private static IEnumerator LoadSprite(string key, Action<string, UnityObject> callback)
+    {
+        yield return AddressableManager.Load<Sprite>(key, callback);
+    }
+
+    private static IEnumerator LoadTexture2D(string key, Action<string, UnityObject> callback)
+    {
+        yield return AddressableManager.Load<Texture2D>(key, callback);
+    }
+#endif
 }
