ab98c9669b9c0fe6a3d4ecb03c95cfd8fddbbeb5
Grashaar <grashaar@outlook.com>
2020-04-30 19:29:49 +0700

    Add ScrollbarBinder

---
+++ UIMan/Scripts/MVVM/Binders/ScrollbarBinder.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnuGames.MVVM
+{
+    [RequireComponent(typeof(Scrollbar))]
+    [DisallowMultipleComponent]
+    public class ScrollbarBinder : BinderBase
+    {
+        protected Scrollbar scrollbar;
+
+        [HideInInspector]
+        public BindingField valueField = new BindingField("Value");
+
+        [HideInInspector]
+        public TwoWayBinding onValueChanged = new TwoWayBinding("On Value Changed");
+
+        [HideInInspector]
+        public FloatConverter valueConverter = new FloatConverter("Value");
+
+        public override void Initialize(bool forceInit)
+        {
+            if (!CheckInitialize(forceInit))
+                return;
+
+            this.scrollbar = GetComponent<Scrollbar>();
+
+            SubscribeOnChangedEvent(this.valueField, OnUpdateValue);
+
+            OnValueChanged_OnChanged(this.onValueChanged);
+            this.onValueChanged.onChanged += OnValueChanged_OnChanged;
+        }
+
+        private void OnUpdateValue(object val)
+        {
+            var value = this.valueConverter.Convert(val, this);
+            this.scrollbar.SetValueWithoutNotify(value);
+        }
+
+        private void OnValueChanged(float value)
+        {
+            SetValue(this.valueField.member, value);
+        }
+
+        private void OnValueChanged_OnChanged(bool value)
+        {
+            this.scrollbar.onValueChanged.RemoveListener(OnValueChanged);
+
+            if (value)
+                this.scrollbar.onValueChanged.AddListener(OnValueChanged);
+        }
+    }
+}
