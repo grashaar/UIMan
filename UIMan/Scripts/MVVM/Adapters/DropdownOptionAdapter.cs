ac5fbba01ff09b306f8f3c459712f4373ba6c894
Grashaar <grashaar@outlook.com>
2020-04-30 19:35:54 +0700

    Add DropdownBinder and related converter, adapter

---
+++ UIMan/Scripts/MVVM/Adapters/DropdownOptionAdapter.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnuGames.MVVM
+{
+    [CreateAssetMenu(menuName = "UIMan/Adapters/Dropdown Option Adapter")]
+    public class DropdownOptionAdapter : Adapter<Dropdown.OptionData>
+    {
+        [SerializeField]
+        private bool forceToString = true;
+
+        private static readonly Dropdown.OptionData _defaultValue = new Dropdown.OptionData(string.Empty);
+
+        public override Dropdown.OptionData Convert(object value, Object context)
+            => Convert(value, this.forceToString, context);
+
+        public static Dropdown.OptionData Convert(object value, bool forceToString, Object context)
+        {
+            if (value == null)
+                return _defaultValue;
+
+            if (!(value is Dropdown.OptionData val))
+            {
+                if (forceToString)
+                {
+                    val = new Dropdown.OptionData(value.ToString());
+                }
+                else
+                {
+                    UnuLogger.LogError($"Cannot convert '{value}' to string.", context);
+                    val = _defaultValue;
+                }
+            }
+
+            return val;
+        }
+
+#if UNITY_EDITOR
+        [UnityEditor.MenuItem("UIMan/Adapters/Dropdown Option Adapter")]
+        private static void CreateDropdownOptionAdapterAsset()
+            => CreateAdapter<DropdownOptionAdapter>(nameof(DropdownOptionAdapter));
+#endif
+    }
+}
---
+++ UIMan/Scripts/MVVM/Converters/DropdownOptionConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine.UI;
+
+namespace UnuGames.MVVM
+{
+    [Serializable]
+    public sealed class DropdownOptionConverter : Converter<Dropdown.OptionData, DropdownOptionAdapter>
+    {
+        public DropdownOptionConverter(string label) : base(label) { }
+
+        public override Dropdown.OptionData Convert(object value, UnityEngine.Object context)
+            => DropdownOptionAdapter.Convert(value, true, context);
+    }
+}
