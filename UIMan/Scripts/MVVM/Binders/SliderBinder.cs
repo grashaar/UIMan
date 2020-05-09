d0e37fa7cfc7bb60a5adfda75dd56e052e6891b4
Grashaar <grashaar@outlook.com>
2020-05-10 00:54:10 +0700

    Add binding fields for Slider.minValue, Slider.maxValue

--- UIMan/Scripts/MVVM/Binders/SliderBinder.cs
+++ UIMan/Scripts/MVVM/Binders/SliderBinder.cs
@@ -10,10 +10,22 @@ namespace UnuGames.MVVM
         protected Slider slider;
 
         [HideInInspector]
+        public BindingField minField = new BindingField("Min");
+
+        [HideInInspector]
+        public BindingField maxField = new BindingField("Max");
+
+        [HideInInspector]
         public BindingField valueField = new BindingField("Value");
 
         [HideInInspector]
         public TwoWayBinding onValueChanged = new TwoWayBinding("On Value Changed");
+
+        [HideInInspector]
+        public FloatConverter minConverter = new FloatConverter("Min");
+
+        [HideInInspector]
+        public FloatConverter maxConverter = new FloatConverter("Max");
 
         [HideInInspector]
         public FloatConverter valueConverter = new FloatConverter("Value");
@@ -25,10 +37,24 @@ namespace UnuGames.MVVM
 
             this.slider = GetComponent<Slider>();
 
+            SubscribeOnChangedEvent(this.minField, OnUpdateMin);
+            SubscribeOnChangedEvent(this.maxField, OnUpdateMax);
             SubscribeOnChangedEvent(this.valueField, OnUpdateValue);
 
             OnValueChanged_OnChanged(this.onValueChanged);
             this.onValueChanged.onChanged += OnValueChanged_OnChanged;
+        }
+
+        private void OnUpdateMin(object val)
+        {
+            var value = this.minConverter.Convert(val, this);
+            this.slider.minValue = value;
+        }
+
+        private void OnUpdateMax(object val)
+        {
+            var value = this.maxConverter.Convert(val, this);
+            this.slider.maxValue = value;
         }
 
         private void OnUpdateValue(object val)
