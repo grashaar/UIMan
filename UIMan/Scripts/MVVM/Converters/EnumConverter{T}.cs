1bd7ae4815ee02d852f1d6a43eb9e70a5d9dbece
Grashaar <grashaar@outlook.com>
2021-03-18 19:00:50 +0700

    Add EnumConverter<T>

---
+++ UIMan/Scripts/MVVM/Converters/EnumConverter{T}.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UnuGames.MVVM
+{
+    [Serializable]
+    public class EnumConverter<T> : Converter<T, EnumAdapter<T>> where T : struct, Enum
+    {
+        public EnumConverter(string label) : base(label) { }
+
+        protected override T ConvertWithoutAdapter(object value, UnityEngine.Object context)
+            => EnumAdapter<T>.Convert(value, true, true, default, context);
+
+        protected override object ConvertWithoutAdapter(T value, UnityEngine.Object context)
+            => EnumAdapter<T>.Convert(value);
+    }
+}
